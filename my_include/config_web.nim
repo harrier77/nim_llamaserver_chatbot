09513d9c5b02c0d85b294ad61e48eec3031e8911
harrier77 <harrier77@gmail.com>
2026-06-04 14:26:03 +0200

    fix

--- my_include/config_web.nim
+++ my_include/config_web.nim
@@ -5,7 +5,7 @@
 # from config.nim, WITHOUT pulling in editor.nim / illwill.
 # ============================================================
 
-import os, strutils, times, osproc, json
+import os, strutils, times,  json
 
 var ExeDir*: string = ""
 
